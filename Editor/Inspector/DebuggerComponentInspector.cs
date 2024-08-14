953f7da1862456aac7f18a1cf88a077ee8d97dca
Blank <wangfj11@foxmail.com>
2024-08-14 11:23:01 +0800

    [增加]1. 增加初版本

---
+++ Editor/Inspector/DebuggerComponentInspector.cs
@@ -0,0 +1,74 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using GameFrameX.Editor;
+using UnityEditor;
+using UnityEngine;
+using GameFrameX.Debugger.Runtime;
+
+namespace GameFrameX.Debugger.Editor
+{
+    [CustomEditor(typeof(DebuggerComponent))]
+    internal sealed class DebuggerComponentInspector : ComponentTypeComponentInspector
+    {
+        private SerializedProperty m_Skin = null;
+        private SerializedProperty m_ActiveWindow = null;
+        private SerializedProperty m_ShowFullWindow = null;
+        private SerializedProperty m_ConsoleWindow = null;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+
+            DebuggerComponent t = (DebuggerComponent)target;
+
+            EditorGUILayout.PropertyField(m_Skin);
+
+            if (EditorApplication.isPlaying && IsPrefabInHierarchy(t.gameObject))
+            {
+                bool activeWindow = EditorGUILayout.Toggle("Active Window", t.ActiveWindow);
+                if (activeWindow != t.ActiveWindow)
+                {
+                    t.ActiveWindow = activeWindow;
+                }
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(m_ActiveWindow);
+            }
+
+            EditorGUILayout.PropertyField(m_ShowFullWindow);
+
+            if (EditorApplication.isPlaying)
+            {
+                if (GUILayout.Button("Reset Layout"))
+                {
+                    t.ResetLayout();
+                }
+            }
+
+            EditorGUILayout.PropertyField(m_ConsoleWindow, true);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        protected override void RefreshTypeNames()
+        {
+            RefreshComponentTypeNames(typeof(IDebuggerManager));
+        }
+
+        protected override void Enable()
+        {
+            m_Skin = serializedObject.FindProperty("m_Skin");
+            m_ActiveWindow = serializedObject.FindProperty("m_ActiveWindow");
+            m_ShowFullWindow = serializedObject.FindProperty("m_ShowFullWindow");
+            m_ConsoleWindow = serializedObject.FindProperty("m_ConsoleWindow");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+﻿## HOMEPAGE
+
+GameFrameX 的 Debugger 资源组件
+
+**Debugger 资源组件 (Debugger Component)** - 提供调试日志相关的看板。
+
+# 使用文档(文档编写于GPT4)
+
+## 注意事项
+
+# 使用方式(任选其一)
+
+1. 直接在 `manifest.json` 的文件中的 `dependencies` 节点下添加以下内容
+   ```json
+      {"com.gameframex.unity.debugger": "https://github.com/GameFrameX/com.gameframex.unity.debugger.git"}
+    ```
+2. 在Unity 的`Packages Manager` 中使用`Git URL` 的方式添加库,地址为：https://github.com/GameFrameX/com.gameframex.unity.debugger.git
+
+3. 直接下载仓库放置到Unity 项目的`Packages` 目录下。会自动加载识别
